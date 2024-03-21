013896a6c1474a47cab8368bc2e697ce8b44e37e
Phill <philltv.gaming@gmail.com>
2024-03-21 02:10:09 +0100

    chore: rename css to scss

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import { useState } from "react";
 import { invoke } from "@tauri-apps/api/tauri";
-import "./App.css";
-
+import "./App.scss";
 
 function App() {
-  return (<div>
-
-  </div>);
+  return (
+    <div id="window">
+      <div className="searchbar-field">
+        <input type="text" className="searchbar" />
+      </div>
+    </div>
+  );
 }
 
 export default App;
