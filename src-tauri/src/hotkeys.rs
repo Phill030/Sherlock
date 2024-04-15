08f00f7717c0f7443d3b27471c8e000dfa512360
Phill <50775241+Phill030@users.noreply.github.com>
2024-04-15 23:25:00 +0200

    feat: focus element on opening window

--- src-tauri/src/hotkeys.rs
+++ src-tauri/src/hotkeys.rs
@@ -39,6 +39,7 @@ pub unsafe fn register_global_hotkey(window: Window) {
                 } else {
                     window.show().unwrap();
                     window.set_focus().unwrap();
+                    window.emit("open_window", {}).unwrap();
                 }
             }
         }
--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,5 +1,6 @@
 import { FormEvent } from "react";
 import "../scss/SearchBar.scss";
+import { listen } from "@tauri-apps/api/event";
 
 export default function SearchBar() {
   async function handleInput(e: FormEvent<HTMLInputElement>) {
@@ -7,10 +8,14 @@ export default function SearchBar() {
     invoke("search_input", { value: e.target.value });
   }
 
+  listen("open_window", (_) => {
+    document.getElementById("text-input")?.focus();
+  });
+
   return (
     <div className="searchbar-field">
       <div className="searchbar-box">
-        <input type="text" className="searchbar" onInput={handleInput} />
+        <input type="text" className="searchbar" id="text-input" onInput={handleInput} />
       </div>
     </div>
   );
